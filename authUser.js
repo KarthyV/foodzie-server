3d9c91a6f2e243a1d8d85339c47c27cc85163f30
KarthyV <Kdon979@gmail.com>
2022-06-10 14:46:03 +0530

    Comments added

--- authUser.js
+++ authUser.js
@@ -4,13 +4,21 @@ const User = require("./userModal");
 
 const authUser = async (req, res, next) => {
   try {
+    // Checking the token if its available from the header
     const userToken = req.header("Authorization").replace("Bearer ", "");
+    // Verifying whether the token is valid
     const decodedToken = jwt.verify(userToken, "mealsSecret");
     console.log(decodedToken);
+
+    // Finding the user from database based on the valid token
     const user = await User.findOne({ _id: decodedToken._id });
+
+    // If not valid
     if (!user) return res.status(404).json("Please authenticate");
     else {
+      // Sending the user back to the client
       req.user = user;
+      // Passing onto the next function
       next();
     }
   } catch (error) {
--- userRoutes.js
+++ userRoutes.js
@@ -5,19 +5,27 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 router.post("/users", (req, res) => {
+  // Creating a new USer
+
   try {
+    // Encrypting the password using bcrypt
     bcrypt.hash(req.body.password, 10, function (err, hash) {
+      // Storing the user data in variable
       const userData = {
         email: req.body.email,
         password: hash,
       };
-      const user = new User(userData);
+      const user = new User(userData); // Creating a user with the stored variable
+      // Creating a new token based on the user id
       const token = jwt.sign({ _id: user._id }, "mealsSecret", {
         expiresIn: "24h",
       });
+      // Storing the token along with the user data
       user.token = token;
+      // Saving it to the database
       user.save();
       console.log(user);
+      // Sending the user data back to the client
       res.send(user);
     });
   } catch (error) {
@@ -26,20 +34,28 @@ router.post("/users", (req, res) => {
   }
 });
 
+// Logging In existing user
+
 router.post("/login", (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body; // Getting the user data
+
+    // Finding the user based from database
     User.findOne({ email: email }, (err, foundUser) => {
       if (err) throw new Error("Invalid Credentials");
       else {
         if (!foundUser) res.json({ message: "User not valid" });
+        // If User found comparing the password using bcrypt
         if (foundUser) {
           bcrypt.compare(password, foundUser.password, (err, result) => {
             if (result === true) {
+              // Once the user data matches database, creating a new token for the user
               const token = jwt.sign({ _id: foundUser._id }, "mealsSecret", {
                 expiresIn: "10h",
               });
+              // Saving the token to the database
               foundUser.token = token;
+              // sending the user data to the client
               res.send(foundUser);
             }
           });
@@ -52,33 +68,48 @@ router.post("/login", (req, res) => {
   }
 });
 
+// With the help of valid token getting the user auto logged In with custom middleware
 router.post("/auto-login", authUser, async (req, res) => {
   res.send(req.user);
 });
 
+// Adding favorites
+
 router.post("/add-favorites", authUser, async (req, res) => {
+  // Getting the Recipe ID and Name from the frontend
   const { recipeId, recipeName } = req.body;
+
+  // Assigning the current user to user variable
   const user = req.user;
+
+  // If Favorites is empty adding the first recipe to list
   if (!user.favorites.length) user.favorites.push({ recipeId, recipeName });
+
+  // If Favorites is not empty, adding favorites by checking if the recipe has already been added
   for (i = 0; i < user.favorites.length; i++) {
     if (user.favorites[i].recipeId === recipeId) {
+      // If recipe already exists, not adding it to favorites
       break;
     } else {
       user.favorites.push({ recipeId, recipeName });
       console.log(user.favorites);
     }
   }
+  // Saving the user again to the database along with favorites
   await user.save();
   res.status(200).send(user);
 });
 
+// Removing favorites from the database
 router.post("/delete-favorites", authUser, async (req, res) => {
+  // Getting the recipe ID for checking if it exists in favorites
   const { recipeId } = req.body;
   const user = req.user;
   console.log(recipeId);
   user.favorites = user.favorites.filter(
     (recipe) => recipe.recipeId !== recipeId
   );
+  // After filtering saving again the user and sending back the updated user to client
   await user.save();
   res.status(200).send(user);
 });
