2054b32eaa992c46b167b84cd4e4047183e625b6
KarthyV <Kdon979@gmail.com>
2022-07-20 11:46:49 +0530

    index comments added

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const userRoutes = require("./userRoutes");
 const cors = require("cors");
-const app = express();
+const app = express(); // Initiating express
 const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config(); // .env configuration
 
 require("./db");
-app.use(cors());
+app.use(cors()); // Applying cors middleware for cross platform configuration
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(userRoutes);
+app.use(express.json()); // Changing the receiving data format from JSON to objects
+app.use(userRoutes); //Route config
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
