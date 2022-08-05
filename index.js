bc87454d8f97563be9224e82686ccb8f893371d8
KarthyV <Kdon979@gmail.com>
2022-08-05 10:11:31 +0530

    cors fix

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ dotenv.config(); // .env configuration
 
 require("./db");
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(cors()); // Applying cors middleware for cross platform configuration
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Changing the receiving data format from JSON to objects
