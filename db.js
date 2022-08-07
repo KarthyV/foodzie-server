3301d9822340b3281a7486d41d5a7764d9a53144
KarthyV <Kdon979@gmail.com>
2022-08-07 19:44:18 +0530

    db file removed

--- db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const url = process.env.MONGO_URL;
-mongoose
-  .connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(() => console.log("Connected"))
-  .catch((err) => console.log(err));
--- index.js
+++ index.js
@@ -1,26 +1,32 @@
+const dotenv = require("dotenv");
+dotenv.config(); // .env configuration
+
 const express = require("express");
 const userRoutes = require("./userRoutes");
 const cors = require("cors");
 const app = express(); // Initiating express
-const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-dotenv.config(); // .env configuration
+const url = process.env.MONGO_URL;
+mongoose
+  .connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
-require("./db");
 app.use(cors()); // Applying cors middleware for cross platform configuration
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Changing the receiving data format from JSON to objects
